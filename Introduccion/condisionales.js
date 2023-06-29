aa25516ca05cd0c5bfa9a36aa577556e17d6d2df
Salomón <115399253+salomoncjl@users.noreply.github.com>
2023-06-28 23:58:44 -0300

    Condicionales (#5)
    * agregando condisionales
    
    * condisionales ejemplos 2
    
    * condisionales v2

---
+++ Introduccion/condisionales.js
@@ -0,0 +1,25 @@
+// Ejemplo 1:
+// const usuario = {
+//     edad: 27,
+//     pais: 'Mexico',
+//     ticket: true,
+// };
+
+// if( usuario.edad > 17){
+//     console.log('El usuario es mayor de edad y puede entrar');
+// } else {
+//     console.log('El usuario no es mayor de edad y no podra entrar');
+// }
+
+// Ejemplo 2:
+const usuario = {
+    edad: 27,
+    pais: 'Mexico',
+    ticket: false,
+};
+
+if(usuario.edad > 17 && usuario.ticket ){
+    console.log('El usuario es mayor de edad y tiene ticket, puede entrar');
+} else {
+    console.log('El usuario no es mayor de edad y no podra entrar');
+}
