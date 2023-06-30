fbe457e5653f25f0c1105da762bfa7d57e0924e7
Salomón <115399253+salomoncjl@users.noreply.github.com>
2023-06-30 00:26:28 -0300

    Funciones (#9)
    * archivos creados
    
    * Funciones

---
+++ Introduccion/funciones.js
@@ -0,0 +1,20 @@
+// Forma 1
+
+// function saludo(){
+//     console.log('hola!');
+// }
+
+// const varejemplo =saludo(); // podes poner una funcion en la variable
+
+// Forma 2
+
+// const saludo = function(){
+//     console.log('Hola!');
+// }
+
+// saludo()
+
+// Forma 3
+
+// const saludo = () => console.log('Hola');
+// saludo()
