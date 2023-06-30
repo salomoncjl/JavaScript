f096bd51099bdd9a1b9b623029092e172bcee3e1
salomon.cjl <contrerassalomon6@gmail.com>
2023-06-29 23:19:26 -0300

    condisionales terminado

--- Introduccion/condisionales.js
+++ Introduccion/condisionales.js
@@ -12,14 +12,49 @@
 // }
 
 // Ejemplo 2:
+// const usuario = {
+//     edad: 27,
+//     pais: 'Mexico',
+//     ticket: false,
+// };
+
+// if(usuario.edad > 17 && usuario.ticket ){
+//     console.log('El usuario es mayor de edad y tiene ticket, puede entrar');
+// } else {
+//     console.log('El usuario no es mayor de edad y no podra entrar');
+// }
+
+// Ejemplo 3 - Anidando condisionales
+
+// const usuario = {
+//     edad: 27,
+//     pais: 'Mexico',
+//     ticket: false,
+// };
+
+// if (usuario.edad >= 18){
+//     if (usuario.ticket){
+//         console.log('El usuario es mayor de edad y tiene ticket.');
+//     } else {
+//         console.log('El usuario es mayor de edad, pero no tiene ticket.');
+//     }
+// } else {
+//     console.log('El usuario es menor de edad.');
+// }
+
+// Ejemplo 4 - elseif
 const usuario = {
     edad: 27,
-    pais: 'Mexico',
+    pais: 'Argentina',
     ticket: false,
 };
 
-if(usuario.edad > 17 && usuario.ticket ){
-    console.log('El usuario es mayor de edad y tiene ticket, puede entrar');
+if(usuario.pais === 'Mexico'){
+    console.log('El usuario es mexicano');
+} else if (usuario.pais === 'Colombia'){
+    console.log('El usuario es de Colombia');
+} else if (usuario.pais === 'Argentina'){
+    console.log('El usuario es de Argentina');
 } else {
-    console.log('El usuario no es mayor de edad y no podra entrar');
+    console.log('El usuario no tiene pais');
 }
