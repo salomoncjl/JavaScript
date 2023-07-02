ec4474cd946832146bb798ffe6f0a71910d7347d
salomon.cjl <contrerassalomon6@gmail.com>
2023-07-01 22:29:56 -0300

    parametros

---
+++ Introduccion/parametros.js
@@ -0,0 +1,25 @@
+// const saludo = (nombre = 'amigo') => {
+//     console.log(`Hola ${nombre}!`)
+// };
+
+// saludo('Carlos');
+// saludo('Alex');
+// saludo('Salo');
+// saludo('Cesar');
+// saludo();
+
+// Multiples parametros
+
+// const operacion = (tipo,numero1, numero2) => {
+//     if (tipo === 'suma'){
+//         console.log(numero1 + numero2);
+//     } else if (tipo === 'resta'){
+//         console.log(numero1 - numero2);
+//     } else{
+//         console.log(numero1 * numero2);
+//     }
+// }
+
+// operacion('suma', 0, 9)
+// operacion('resta',1, 2)
+// operacion('multiplo', 3, 4)
