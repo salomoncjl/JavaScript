ca6fce1f2883bdf046d4975b6983a7d94e8964d5
Salomón <115399253+salomoncjl@users.noreply.github.com>
2023-07-06 02:44:30 -0300

    Metodos numeros (#19)
    * Archivos creados
    
    * Metodos para numeros

---
+++ Introduccion/MetodosParaNumeros.js
@@ -0,0 +1,66 @@
+/*
+    .toString()
+*/
+// const numero = 10;
+// console.log(numero, typeof numero);
+
+// const texto = numero.toString();
+// console.log(texto, typeof texto);
+
+/*
+    .toFixed()
+    Permite obtener un numero con la cantidad de decimales especificados
+*/
+// const numero = 3.1415
+// console.log(numero.toFixed(1));// me muestra 1 decimal en este ejemplo
+
+/*
+    .parseInt()
+    Intenta trasformar un calor aun entero.
+*/
+// const num1 = parseInt(prompt('Escribe un numero'));
+// const num2 = parseInt(prompt('Escribe un numero'));
+
+// console.log(num1 + num2);
+
+/*
+    .parseFloat()
+    Intenta transformar un valor a un numero con decimal.
+*/
+// const num1 = parseFloat(prompt('Escribe un numero'));
+// const num2 = parseFloat(prompt('Escribe un numero'));
+
+// console.log(num1 + num2);
+
+/*
+    Math.random()
+    Genera un numero al azar entre 0 y 1
+*/
+// const num = Math.random()
+// console.log(num);
+
+/*
+    Math.floor()
+    Redondea hacia abajo un numero.
+*/
+// console.log(Math.floor('10.1'));
+// console.log(Math.floor('10.99'));
+
+/*
+    Math.ceil()
+*/
+// console.log(Math.ceil('10.1'));
+// console.log(Math.ceil('10.99'));
+
+/*
+    Math.round()
+    Redondea havia el entero mas cercano
+*/
+// console.log(Math.round('10.1'));
+// console.log(Math.round('10.99'));
+
+/*
+    Ejemplo de numero al azar de 0 a 100
+*/
+// const num = Math.random();
+// console.log(Math.floor(num * 101));
