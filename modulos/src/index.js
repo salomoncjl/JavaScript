1ac981f1411ccda8277574fc8c77c0f65fcde609
Salomón <115399253+salomoncjl@users.noreply.github.com>
2023-07-19 23:56:06 -0300

    Rollup practica (#32)
    * index y style agregados
    
    * Forma de exportar1

--- modulos/src/index.js
+++ modulos/src/index.js
@@ -1,4 +1,14 @@
-import carrito from './carrito.js';
+// Forma de importar - Named Imports
 
-console.log('Archivo incial');
-carrito();
+// import {nombre as nombreImportado, obtenerPost} from '../src/nameExports';
+
+// //el nombre as nombreImportado es para cambiar el alias, por si se llega a tener una variable con el mismo nombre que el import
+
+// console.log('mi nombre es ' + nombreImportado);
+// console.log(obtenerPost());
+
+// Forma de importar - Namespace Imports
+import * as datos from './nameExports'
+
+console.log(datos.nombre);
+console.log(datos.obtenerPost());
