26dee38a3cb1ef295d983b1194cce58634178b8d
Salomón <115399253+salomoncjl@users.noreply.github.com>
2023-07-21 22:07:21 -0300

    Imports (#34)
    * Export default
    
    * export tipos

--- modulos/src/defaultExport.js
+++ modulos/src/defaultExport.js
@@ -1,7 +1,20 @@
-export default  () => {
+// Forma 1
+
+// export default  () => {
+//     // console.log('hola');
+//     return {
+//         nombre: 'Carlos',
+//         correo: 'correo@correo.com'
+//     }
+// }
+
+// Forma 2
+
+const obtenerUsuario = () => {
+    // console.log('hola');
     return {
         nombre: 'Carlos',
         correo: 'correo@correo.com'
     }
 }
-
+export default obtenerUsuario
--- modulos/src/index.js
+++ modulos/src/index.js
@@ -1,4 +1,4 @@
-// Forma de importar - Named Imports
+// 1 Forma de importar - Named Imports
 
 // import {nombre as nombreImportado, obtenerPost} from '../src/nameExports';
 
@@ -7,12 +7,14 @@
 // console.log('mi nombre es ' + nombreImportado);
 // console.log(obtenerPost());
 
-// Forma de importar - Namespace Imports
+// 2 Forma de importar - Namespace Imports
 // import * as datos from './nameExports'
 
 // console.log(datos.nombre);
 // console.log(datos.obtenerPost());
 
 // Default Imports
-import cualquierNombrePongo from './defaultExport'
-console.log(cualquierNombrePongo());
+// import cualquierNombrePongo from './defaultExport'
+// console.log(cualquierNombrePongo());
+
+// 3 Forma Empty Imports
