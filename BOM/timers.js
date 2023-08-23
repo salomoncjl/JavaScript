b4312f77ccce6b538aad861b3d0e86a1d2b60e0f
salomon.cjl <contrerassalomon6@gmail.com>
2023-08-22 23:45:33 -0300

    timers

--- BOM/timers.js
+++ BOM/timers.js
@@ -1,27 +1,48 @@
 /*
-    window.setTimeOut()
+    📌 window.setTimeOut()
     Nos pérmite ejecutar una funcion despues de cierto tiempo.
 */
-// Forma 1
+// 🤯 Forma 1
 // setTimeout(() => {
 //         console.log('hola');
 // }, 5000)
 
 
-// Forma 2 
-const saludo = () => {
-    console.log('Hola! no boton');
-};
-setTimeout(saludo, 5000)
+// 🤯 Forma 2 
+// const saludo = () => {
+//     console.log('Hola! no boton');
+// };
+// setTimeout(saludo, 5000)
 
-// Forma con boton
-let id;
+// 🤯 Forma con boton
+// let id;
+// const iniciar = () => {
+//     console.log('Iiciando timer');
+//     id = setTimeout(saludo, 5000);
+// };
+
+// const parar = () => {
+//     console.log('Parando timer');
+//     clearTimeout(id);
+// }
+
+/*
+    📌 window.setInterval()
+    Nos permite ejecutar una funcion cada cierto tiempo.
+*/
+//
+let cuenta = 0;
+let id
+
 const iniciar = () => {
-    console.log('Iiciando timer');
-    let id = setTimeout(saludo, 5000)
-};
+    id = setInterval(() => {
+        console.log(cuenta);
+        cuenta++
+    }, 1000);
+}
 
 const parar = () => {
-    console.log('Parando timer');
-    clearTimeout(id)
+    console.log('Paramos la cuenta');
+    clearInterval(id)
 }
+
