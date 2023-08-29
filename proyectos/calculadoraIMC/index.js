c2df648f6f06bb566bc0d6919001ad31ea9a7be1
Salomón <115399253+salomoncjl@users.noreply.github.com>
2023-08-28 23:55:57 -0300

    Calculadora imc (#47)
    * Calculadora IBM crecaion de archivos
    
    * normalizacion
    
    * formato html listo
    
    * css estilado falta
    
    * estilos terminados
    
    * javascript calculo

--- proyectos/calculadoraIMC/index.js
+++ proyectos/calculadoraIMC/index.js
@@ -1,1 +1,20 @@
-// alert('funciona')
+// Obtener referencia a los elementos HTML
+const pesoInput = document.getElementById('peso');
+const alturaInput = document.getElementById('altura');
+const calcularBtn = document.getElementById('calcular');
+const resultadoDiv = document.getElementById('resultado');
+
+// Funcion para calcular el IMC
+function calcurIMC() {
+    const peso = parseFloat(pesoInput.value);
+    const altura = parseFloat(alturaInput.value)
+
+    if (isNaN(peso) || isNaN(altura) || altura === 0) {
+        resultadoDiv.textContent = 'Por favor, ingrese valores valido de peso y altura.'
+        return;
+    }
+    const imc = peso / (altura ** 2);
+    resultadoDiv.textContent = `Tu IMC es: ${imc.toFixed(2)}`
+}
+
+// calcularBtn.addEventListener('click', calcurIMC)
