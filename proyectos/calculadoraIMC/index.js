3b31bc3c0453f1ffb020e42a5104e9d66a8cdfbd
salomon.cjl <contrerassalomon6@gmail.com>
2023-08-30 23:54:11 -0300

    Ultimo fix del IMC

--- proyectos/calculadoraIMC/index.js
+++ proyectos/calculadoraIMC/index.js
@@ -17,9 +17,9 @@ function calcurIMC() {
     
     if( imc <= 18.5){
         resultadoDiv.textContent = `Tu IMC es: ${imc.toFixed(2)} - Peso bajo (Delgado).`
-    } else if( imc <= 18.6){
+    } else if( imc >= 18.6 && imc <= 24.8){
         resultadoDiv.textContent = `Tu IMC es: ${imc.toFixed(2)} - Normal.`
-    } else if( imc <= 25){
+    } else if( imc >= 24.9 && imc <= 29.9){
         resultadoDiv.textContent = `Tu IMC es: ${imc.toFixed(2)} - Sobrepeso.`
     } else {
         resultadoDiv.textContent = `Tu IMC es: ${imc.toFixed(2)} - Obesidad.`
