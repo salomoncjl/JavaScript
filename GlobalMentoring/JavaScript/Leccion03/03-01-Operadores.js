3b536d0d49b2f36d0ea89adf01fba5129f2ab577
salomon.cjl <contrerassalomon6@gmail.com>
2023-09-29 00:54:44 -0300

    Operadores aritmeticos

---
+++ GlobalMentoring/JavaScript/Leccion03/03-01-Operadores.js
@@ -0,0 +1,19 @@
+let a = 3, b = 2;
+let z = a + b;
+console.log('El resultado de la suma: ' + z);
+
+z = a - b;
+console.log('El resultado de la resta: ' +z);
+
+z = a * b;
+console.log('El resultado de la multiplicacion: ' + z);
+
+z = a / b;
+console.log('El resultado de la division: ' + z);
+
+z = a % b;
+console.log('El resultado del resto: ' + z);
+
+z = a ** b;
+console.log('El resultado de la potencia: ' + z);
+
