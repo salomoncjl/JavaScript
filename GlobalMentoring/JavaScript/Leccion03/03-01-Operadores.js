d6c87fd4905324f720141bc7f6daf9dba5ce3bd2
salomon.cjl <contrerassalomon6@gmail.com>
2023-10-03 23:43:02 -0300

    incrementos

--- GlobalMentoring/JavaScript/Leccion03/03-01-Operadores.js
+++ GlobalMentoring/JavaScript/Leccion03/03-01-Operadores.js
@@ -1,19 +1,30 @@
 let a = 3, b = 2;
 let z = a + b;
-console.log('El resultado de la suma: ' + z);
+// console.log('El resultado de la suma: ' + z);
 
-z = a - b;
-console.log('El resultado de la resta: ' +z);
+// z = a - b;
+// console.log('El resultado de la resta: ' +z);
 
-z = a * b;
-console.log('El resultado de la multiplicacion: ' + z);
+// z = a * b;
+// console.log('El resultado de la multiplicacion: ' + z);
 
-z = a / b;
-console.log('El resultado de la division: ' + z);
+// z = a / b;
+// console.log('El resultado de la division: ' + z);
 
-z = a % b;
-console.log('El resultado del resto: ' + z);
+// z = a % b;
+// console.log('El resultado del resto: ' + z);
 
-z = a ** b;
-console.log('El resultado de la potencia: ' + z);
+// z = a ** b;
+// console.log('El resultado de la potencia: ' + z);
 
+// Incremento
+
+// Pre-incremento 
+// z = ++a;
+// console.log(a);
+// console.log(z);
+
+// Post-incremento
+z = b++;
+console.log(b);
+console.log(z);
