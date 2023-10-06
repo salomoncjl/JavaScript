26190aaa8e10b889e26dc1a8eb6c1908bcbe5f25
salomon.cjl <contrerassalomon6@gmail.com>
2023-10-06 02:55:14 -0300

    fix

--- GlobalMentoring/JavaScript/Leccion03/03-01-Operadores.js
+++ GlobalMentoring/JavaScript/Leccion03/03-01-Operadores.js
@@ -52,6 +52,16 @@
 
 
 // Operador de asignacion
-let a = 1;
-a += 3; // a = a + 3
-console.log(a);
+// let a = 1;
+// a += 3; // a = a + 3
+// console.log(a);
+
+// a -= 2;
+// console.log(a);
+
+/*
+    *=
+    /=
+    %=
+    **=
+*/
