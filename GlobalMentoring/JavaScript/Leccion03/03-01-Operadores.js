921d508c2b1046ce4e7c12c7247e84bc826fe2cc
salomon.cjl <contrerassalomon6@gmail.com>
2023-10-10 14:21:03 -0300

    ejercicio edad

--- GlobalMentoring/JavaScript/Leccion03/03-01-Operadores.js
+++ GlobalMentoring/JavaScript/Leccion03/03-01-Operadores.js
@@ -91,10 +91,20 @@
 // console.log(x);
 // console.log(y);
 // console.log(o);
-let a = 10;
 
-if( a % 2 == 0){
-    console.log('Es un numero par');
-} else {
-    console.log('Es un numero impar');
+// Ejercicio Par o Impar
+// let a = 9;
+
+// if( a % 2 == 0){
+//     console.log('Es un numero par');
+// } else {
+//     console.log('Es un numero impar');
+// }
+
+// Ejercicio Edad
+let edad = 20, adulto = 18;
+if(edad >= adulto){
+    console.log("Es un adulto");
+} else{
+    console.log("Es menor de edad");
 }
