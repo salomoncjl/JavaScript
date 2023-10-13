011a590b8758582643f52e48c6086645dbcd167d
salomon.cjl <contrerassalomon6@gmail.com>
2023-10-13 01:54:22 -0300

    Ejemplo sentencia if else

--- GlobalMentoring/JavaScript/Leccion04/04-01-SentenciasControl-JS.js
+++ GlobalMentoring/JavaScript/Leccion04/04-01-SentenciasControl-JS.js
@@ -1,7 +1,22 @@
-let condicion = true;
+// Sentencia if else
+// let condicion = true;
 
-if (condicion) {
-    console.log('Condicion verdadera');
-} else {
-    console.log('Condicion falsa');
+// if (condicion) {
+//     console.log('Condicion verdadera');
+// } else {
+//     console.log('Condicion falsa');
+// }
+
+// Ejemplo
+let numero = "3";
+if (numero == 1) {
+    console.log("Numero uno");
+} else if (numero == 2){
+    console.log("Numero dos");
+} else if (numero == 3){
+    console.log('Numero tres');
+} else if (numero == 4){
+    console.log('Numero cuatro');
+} else{
+    console.log('Numero no encontrado');
 }
