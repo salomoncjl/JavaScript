0ef432301464f32bc73969ee1c73ddadd3cc6992
salomon.cjl <contrerassalomon6@gmail.com>
2023-10-18 18:46:56 -0300

    Ejemplo con dia de la semana

--- GlobalMentoring/JavaScript/Leccion04/04-01-SentenciasControl-JS.js
+++ GlobalMentoring/JavaScript/Leccion04/04-01-SentenciasControl-JS.js
@@ -37,3 +37,26 @@ if ( mes == 12 || mes == 1 || mes == 2 ) {
     estacion = "Valor incorrecto"
 }
 console.log(estacion);
+
+
+// Dia de la semana
+let diaSemana = 1;
+
+if ( diaSemana == 1) {
+    console.log('Lunes');
+} else if( diaSemana == 2 ){
+    console.log('Martes'); 
+} else if( diaSemana == 3 ){
+    console.log('Miercoles'); 
+} else if( diaSemana == 4 ){
+    console.log('Jueves'); 
+} else if( diaSemana == 5){
+    console.log('Viernes');
+} else if( diaSemana == 6){
+    console.log('Sabado');
+} else if( diaSemana == 7){
+    console.log('Domingo');
+} else {
+    console.log('Dia de semana invalido');
+}
+console.log(estacion);
