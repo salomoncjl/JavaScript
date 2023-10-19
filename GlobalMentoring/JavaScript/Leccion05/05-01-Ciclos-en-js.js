7601d57ddb56a7ed298aca8604dbc8fe8a867977
salomon.cjl <contrerassalomon6@gmail.com>
2023-10-19 00:32:19 -0300

    Ciclo do While

--- GlobalMentoring/JavaScript/Leccion05/05-01-Ciclos-en-js.js
+++ GlobalMentoring/JavaScript/Leccion05/05-01-Ciclos-en-js.js
@@ -1,10 +1,18 @@
 // Ciclo while
 // Imprimir los calores de 0 al 5;
 
+// let contador = 0;
+// let repeticiones = 5;
+
+// while (contador <= repeticiones) {
+//     console.log(contador++);
+//     // contador++ // eso o lo de arriba
+// }
+
+// Ciclo while do
 let contador = 0;
 let repeticiones = 5;
 
-while (contador <= repeticiones) {
+do{
     console.log(contador++);
-    // contador++ // eso o lo de arriba
-}
+} while( contador <= repeticiones)
