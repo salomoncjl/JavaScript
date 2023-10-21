16598993f1a6af55ccfc6cd83dec374bdee96554
salomon.cjl <contrerassalomon6@gmail.com>
2023-10-20 21:03:57 -0300

    ejemplo dejercicio for

---
+++ GlobalMentoring/JavaScript/Leccion05/Ejercicio-practico-3-de-3.js
@@ -0,0 +1,13 @@
+// Imprimir los primeros 10 numeros de 3 en 3
+let numero = 0
+// 1 - 4 - 7 - 10 - 13
+
+for(numero; numero <= 10 ; numero += 3){
+    console.log(numero);
+}
+
+// 1 - -2 - -5 - -8 - -11
+
+for(numero; -10 <= numero ; numero -= 3){
+    console.log(numero);
+}
